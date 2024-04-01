f361f7e64128514948386904d4db6c41c1008e0a
NRS08 <nirantsontakke@gmail.com>
2024-04-01 23:08:11 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/pages/LandingPage";
+import "./App.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "./components/pages/Navbar";
--- src/components/utils/PriceWrapperCard.jsx
+++ src/components/utils/PriceWrapperCard.jsx
@@ -1,50 +1,53 @@
-import React from 'react'
+import React from "react";
 import {
-    Box,
-    Stack,
-    HStack,
-    Heading,
-    Text,
-    VStack,
-    useColorModeValue,
-    List,
-    ListItem,
-    ListIcon,
-    Button,
-  } from '@chakra-ui/react'
-  import { FaCheckCircle } from "react-icons/fa";
-  import { useNavigate } from 'react-router-dom';
-
+  Box,
+  Stack,
+  HStack,
+  Heading,
+  Text,
+  VStack,
+  useColorModeValue,
+  List,
+  ListItem,
+  ListIcon,
+  Button,
+} from "@chakra-ui/react";
+import { FaCheckCircle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function PriceWrapperCard({ policy }) {
-    const navigate= useNavigate()
-    let { name, company, coverage, maxAmount, insuranceId, _id, price } = policy;
-    let str = coverage.toString();
-   
-function PriceWrapper(props) {
+  const navigate = useNavigate();
+  let { name, company, coverage, maxAmount, insuranceId, _id, price } = policy;
+  let str = coverage.toString();
+  const search = () => {
+    navigate(`/policy/${_id}`, { state: _id });
+  };
 
-    const { children } = props
-  
+  function PriceWrapper(props) {
+    const { children } = props;
+
     return (
       <Box
         mb={4}
         shadow="base"
         borderWidth="1px"
-        alignSelf={{ base: 'center', lg: 'flex-start' }}
-        borderColor={useColorModeValue('gray.200', 'gray.500')}
-        borderRadius={'xl'}>
+        alignSelf={{ base: "center", lg: "flex-start" }}
+        borderColor={useColorModeValue("gray.200", "gray.500")}
+        borderRadius={"xl"}
+      >
         {children}
       </Box>
-    )
+    );
   }
-    
+
   return (
-    <PriceWrapper>
-          <Box py={4} px={12}>
-            <Text fontWeight="500" fontSize="2xl">
-              {name}
-            </Text>
-            {/* <HStack justifyContent="center">
+    <Box onClick={search} cursor={"pointer"}>
+      <PriceWrapper>
+        <Box py={4} px={12}>
+          <Text color={"red.400"} fontWeight="600" fontSize="2xl">
+            {name}
+          </Text>
+          {/* <HStack justifyContent="center">
               <Text fontSize="3xl" fontWeight="600">
                 $
               </Text>
@@ -55,48 +58,50 @@ function PriceWrapper(props) {
                 /month
               </Text>
             </HStack> */}
-          </Box>
-          <VStack
-            bg={useColorModeValue('gray.50', 'gray.700')}
-            py={4}
-            borderBottomRadius={'xl'}
-            textTransform={"capitalize"}>
-            <List spacing={3} textAlign="start" px={12}>
-               {insuranceId &&
-                <ListItem>
+        </Box>
+        <VStack
+          bg={useColorModeValue("gray.50", "gray.700")}
+          py={4}
+          borderBottomRadius={"xl"}
+          textTransform={"capitalize"}
+        >
+          <List spacing={3} textAlign="start" px={12}>
+            {insuranceId && (
+              <ListItem>
                 <ListIcon as={FaCheckCircle} color="green.500" />
                 {`ID: ${insuranceId}`}
               </ListItem>
-            }
+            )}
+            <ListItem>
+              <ListIcon as={FaCheckCircle} color="green.500" />
+              {`Company: ${company}`}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={FaCheckCircle} color="green.500" />
+              {`Coverage: ${str}`}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={FaCheckCircle} color="green.500" />
+              {`Max Coverage: Rs ${maxAmount}`}
+            </ListItem>
+            {price && (
               <ListItem>
-                <ListIcon as={FaCheckCircle} color="green.500" />
-                {`Company: ${company}`}
-              </ListItem>
-              <ListItem>
-                <ListIcon as={FaCheckCircle} color="green.500" />
-                {`Coverage: ${str}`}
-              </ListItem>
-              <ListItem>
-                <ListIcon as={FaCheckCircle} color="green.500" />
-                {`Max Coverage: Rs ${maxAmount}`}
-              </ListItem>
-              { price && 
-                  <ListItem>
                 <ListIcon as={FaCheckCircle} color="green.500" />
                 {`Premium: Rs ${price} / annually`}
               </ListItem>
-            }
-            </List>
-            <Box w="80%" pt={7}>
-              {/* <Button  onClick={() => {
+            )}
+          </List>
+          <Box w="80%" pt={7}>
+            {/* <Button  onClick={() => {
                 navigate("/login");
                 }} w="full" colorScheme="green" variant="outline">
                 Get Started
               </Button> */}
-            </Box>
-          </VStack>
-        </PriceWrapper>
-  )
+          </Box>
+        </VStack>
+      </PriceWrapper>
+    </Box>
+  );
 }
 
-export default PriceWrapperCard
+export default PriceWrapperCard;
